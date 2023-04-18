aea634c5a9263a166a0c8b44d42a278a0603b336
user.email <gordeev@ppdp.ru>
2023-04-18 10:10:53 +0300

    work with __name__ == "__main__"

---
+++ bin/import_module_from.py
@@ -0,0 +1,7 @@
+def some_func(n):
+    return n*10
+
+
+if __name__ == "__main__":
+    a = some_func(8)
+    print(a)
---
+++ bin/import_module_to.py
@@ -0,0 +1,5 @@
+from bin.import_module_from import some_func
+
+
+a = some_func(10)
+print(a)
